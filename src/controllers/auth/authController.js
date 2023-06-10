9f62e2b62ae2d5b1e56919ecd562432b84c1296d
Jonatank28 <jonatankaleidakk28@gmail.com>
2023-06-10 16:12:26 -0300

    feat: ajustado token de acesso

--- src/controllers/auth/authController.js
+++ src/controllers/auth/authController.js
@@ -8,71 +8,88 @@ class AuthController {
     async login(req, res) {
         const { email, password } = req.body;
 
-        const sqlVerifyUser = `SELECT * FROM user WHERE email = ?`;
-        const [user] = await db.promise().query(sqlVerifyUser, email);
+        try {
+            const sqlVerifyUser = `SELECT * FROM user WHERE email = ?`;
+            const [user] = await db.promise().query(sqlVerifyUser, email);
 
-        if (user.length === 0) {
-            return res.status(400).json({ message: 'Senha e/ou email inválidos' });
+            if (user.length === 0) {
+                return res.status(400).json({ message: 'Senha e/ou email inválidos' });
+            }
+
+            const verifyPassword = await bcrypt.compare(password, user[0].password);
+
+            if (!verifyPassword) {
+                return res.status(400).json({ message: 'Senha e/ou email inválidos' });
+            }
+
+            const token = jwt.sign({ id: user[0].userID }, process.env.JWT_PASS ?? '', { expiresIn: '8h' });
+
+            const { password: _, ...userLogin } = user[0];
+
+            return res.status(200).json({
+                user: userLogin,
+                token: token
+            });
+        } catch (error) {
+            // console.log(error);
+            return res.status(500).json({ message: 'Erro ao realizar login' });
         }
-
-        const verifyPassword = await bcrypt.compare(password, user[0].password);
-
-        if (!verifyPassword) {
-            return res.status(400).json({ message: 'Senha e/ou email inválidos' });
-        }
-
-        const token = jwt.sign({ id: user[0].userID }, process.env.JWT_PASS ?? '', { expiresIn: '8h' });
-
-        const { password: _, ...userLogin } = user[0]
-
-        return res.status(200).json({
-            user: userLogin,
-            token: token
-        })
     }
+
 
     //! Verifica se o token do usuário é válido
     async loginVerifyToken(req, res) {
-        const { authorization } = req.headers
+        const authorization = req.headers.authorization;
 
         if (!authorization) {
-            return res.status(401).json({ message: "Não autorizado" })
+            return res.status(401).json({ message: "Não autorizado" });
         }
 
-        const token = authorization.split(" ")[1]
+        const token = authorization.split(" ")[1];
 
-        const { id } = jwt.verify(token, process.env.JWT_PASS ?? '')
+        try {
+            const { id } = jwt.verify(token, process.env.JWT_PASS);
+            const sqlVerifyUser = `SELECT * FROM user WHERE userID = ?`;
+            const [user] = await db.promise().query(sqlVerifyUser, id);
 
-        const sqlVerifyUser = `SELECT * FROM user WHERE userID = ?`;
-        const [user] = await db.promise().query(sqlVerifyUser, id);
+            if (user.length === 0) {
+                return res.status(401).json({ message: "Não autorizado" });
+            }
 
-        if (user.length === 0) {
-            return res.status(400).json({ message: "Não autorizado" });
+            const { password: _, ...loggedUser } = user[0];
+
+            return res.status(200).json(loggedUser);
+        } catch (error) {
+            // console.log("Token inválido");
+            return res.status(401).json({ message: "Não autorizado" });
         }
-
-        const { password: _, ...loggedUuser } = user[0]
-
-        return res.status(200).json(loggedUuser)
     }
+
 
     //! Faz registro de um novo usuário
     async register(req, res) {
         const { name, email, password } = req.body;
 
-        const sqlVerifyEmail = `SELECT * FROM user WHERE email = ?`;
-        const [user] = await db.promise().query(sqlVerifyEmail, email);
+        try {
+            const sqlVerifyEmail = `SELECT * FROM user WHERE email = ?`;
+            const [user] = await db.promise().query(sqlVerifyEmail, email);
 
-        if (user.length > 0) {
-            return res.status(400).json({ message: 'Email já cadastrado' });
+            if (user.length > 0) {
+                return res.status(400).json({ message: 'Email já cadastrado' });
+            }
+
+            const hashPassword = await bcrypt.hash(password, 10);
+
+            const newUser = `INSERT INTO user (name, email, password) VALUES (?, ?, ?)`;
+            const [result] = await db.promise().query(newUser, [name, email, hashPassword]);
+
+            return res.status(201).json({ message: 'Usuário criado com sucesso' });
+        } catch (error) {
+            // console.log(error);
+            return res.status(500).json({ message: 'Erro ao criar usuário' });
         }
-
-        const hashPassword = await bcrypt.hash(password, 10);
-
-        const newUSer = `INSERT INTO user (name, email, password) VALUES (?, ?, ?)`
-        const [result] = await db.promise().query(newUSer, [name, email, hashPassword]);
-
-        return res.status(201).json({ message: 'Usuário criado com sucesso' });
     }
+
 }
 
 module.exports = AuthController;
