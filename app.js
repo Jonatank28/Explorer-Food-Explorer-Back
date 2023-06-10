1b53f05c84f9e9286ac2a2934d981beec551c7bf
Jonatank28 <jonatankaleidakk28@gmail.com>
2023-06-09 23:10:53 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+// const routes = require("./routes");
+
+app.use(express.json());
+app.use(cors({ origin: '*' }));
+// app.use(routes);
+
+
+app.listen(3333, () => {
+    console.log('Server is running on port 3333');
+});
